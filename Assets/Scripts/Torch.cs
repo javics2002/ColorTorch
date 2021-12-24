dc936b1c9ee3d1519b6e20e2028015acc92d8f22
javics2002 <49459590+javics2002@users.noreply.github.com>
2021-12-24 18:17:20 +0100

    GDD and Project

---
+++ Assets/Scripts/Torch.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Torch : MonoBehaviour
+{
+    Light torch;
+
+    void Start()
+    {
+        torch = GetComponentInChildren<Light>();
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            torch.color = Color.red;
+        }
+    }
+}
